8d38b174e20750f1cb51ce90aed718c24cce9346
Dylan <dcren2010@gmail.com>
2021-08-30 18:03:20 -0400

    add new line to index html

--- rock-paper-scissors-web/main.go
+++ rock-paper-scissors-web/main.go
@@ -9,25 +9,20 @@ import (
 	"strconv"
 )
 
-// Home page
 func homePage(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "index.html")
-
 }
 
 func playRound(w http.ResponseWriter, r *http.Request) {
-	// convert string to int
-	playerChoice, _ := strconv.Atoi(r.URL.Query().Get("a"))
-	
+	playerChoice, _ := strconv.Atoi(r.URL.Query().Get("c"))
 	result := rps.PlayRound(playerChoice)
 
-	out, err := json.MarshalIndent(result, "", "   ")
-
+	out, err := json.MarshalIndent(result, "", "    ")
 	if err != nil {
-		log.Panicln(err)
+		log.Println(err)
 		return
 	}
-	w.Header().Set("Content/Type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.Write(out)
 }
 
@@ -35,21 +30,18 @@ func main() {
 	http.HandleFunc("/play", playRound)
 	http.HandleFunc("/", homePage)
 
-	// Build up the server
 	log.Println("Starting web server on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
 
-// Function for all templates, it has two parameters: http.ResponseWriter, and page
 func renderTemplate(w http.ResponseWriter, page string) {
-	// t for template
 	t, err := template.ParseFiles(page)
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	err = t.Execute(w, nil) 
+	err = t.Execute(w, nil)
 	if err != nil {
 		log.Println(err)
 		return
--- rock-paper-scissors-web/rps/rps.go
+++ rock-paper-scissors-web/rps/rps.go
@@ -9,19 +9,18 @@ const (
 	ROCK     = 0 // beats scissors. (scissors + 1) % 3 = 0
 	PAPER    = 1 // beats rock. (rock + 1) % 3 = 1
 	SCISSORS = 2 // beats paper. (paper + 1) % 3 = 2
-
-	// *** next three lines not used
-	// PLAYERWINS 		= 1
-	// COMPUTERWINS 	= 2
-	// DRAW 					= 3
+	// *** next three lines not used, and can be deleted
+	// PLAYERWINS   = 1
+	// COMPUTERWINS = 2
+	// DRAW         = 3
 )
 
 type Round struct {
-	// Winner 					int 		`json:"winner"`
-
-	Message 				string  `json:"message"`
-	ComputerChoice 	string 	`json:"computer_choice"`
-	RoundResult 		string 	`json:"round_result"`
+	// *** changed Winner to Message
+	// Winner         int    `json:"winner"`
+	Message        string `json:"message"`
+	ComputerChoice string `json:"computer_choice"`
+	RoundResult    string `json:"round_result"`
 }
 
 // *** created three slices of strings, each with exactly three entries
@@ -43,23 +42,24 @@ var drawMessages = []string{
 	"Nobody wins, but you can try again.",
 }
 
-// return round number, computerScore, roundResult
 func PlayRound(playerValue int) Round {
 	rand.Seed(time.Now().UnixNano())
 	computerValue := rand.Intn(3)
 	computerChoice := ""
 	roundResult := ""
-
 	// *** delete next line, since the variable is no longer being used
 	//winner := 0
 
 	switch computerValue {
 	case ROCK:
 		computerChoice = "Computer chose ROCK"
+		break
 	case PAPER:
 		computerChoice = "Computer chose PAPER"
+		break
 	case SCISSORS:
 		computerChoice = "Computer chose SCISSORS"
+		break
 	default:
 	}
 
@@ -84,9 +84,9 @@ func PlayRound(playerValue int) Round {
 		// *** populate message from loseMessages
 		message = loseMessages[messageInt]
 	}
-	
-	var result Round;
-	// result.Winner = winner
+
+	var result Round
+	// *** change to use message instead of Winner
 	result.Message = message
 	result.ComputerChoice = computerChoice
 	result.RoundResult = roundResult
