00172e0b4d2c6ce7ea36daa44fb0076596a01a58
Dylan <dcren2010@gmail.com>
2021-08-30 15:42:31 -0400

    build html page and template render function

--- main.go
+++ main.go
@@ -1,20 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
 // Home page
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!")
+	renderTemplate(w, "index.html")
+
 }
 
 func main() {
-
 	// Build up the server:
 	http.HandleFunc("/", homePage)
 	log.Println("Starting web server on port 8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+// Function for all templates, it has two parameters: http.ResponseWriter, and page
+func renderTemplate(w http.ResponseWriter, page string) {
+	// t for template
+	t, err := template.ParseFiles(page)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	err = t.Execute(w, nil) 
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
